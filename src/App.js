af36392d272a4ba10cd056564bcb533d464721d3
Ronaldo-007 <azizjonaxmadaliyev@gmail.com>
2021-07-10 16:25:49 +0500

    fayil

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from '.file:///home/muhammadali/Downloads/Telegram%20Desktop/images';
 import './App.css';
+import logo from "./logo.svg"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={"file:///home/muhammadali/Downloads/Telegram%20Desktop/images"} className="App-logo" alt="logo" />
-        <p>
-         Salom xammaga do'stlarim
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="shadow">
+       <div className="container py-3">
+       <img src={logo} className="logo" alt="" />
+       </div>
       </header>
     </div>
   );
